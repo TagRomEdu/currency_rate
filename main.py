01b66d745042381c5725efc07f34a67fc1779430
TagRomEdu <qwertystyles@gmail.com>
2023-07-08 13:02:35 +0500

    added main func

--- main.py
+++ main.py
@@ -32,3 +32,30 @@ def save_to_json(data: dict) -> None:
             data_lst.append(data)
             with open(CURRENCY_RATES_FILE, 'w') as new_file:
                 json.dump([data], new_file)
+
+
+def main():
+    while True:
+        currency = input("Введите название валюты (USD или EUR): ").upper()
+        if currency not in ("USD", "EUR"):
+            print("Некорректный ввод")
+            continue
+
+        rate = get_currency_rate(currency)
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"Курс {currency} к рублю: {rate:.2f}")
+
+        data = {"rate": rate, "timestamp": timestamp}
+        save_to_json(data)
+
+        choice = input("Выберите действие: (1 - продолжить, 2 - выйти)\n")
+        if choice == 1:
+            continue
+        elif choice == 2:
+            break
+        else:
+            print("Incorrect input")
+
+
+if __name__ == "__main__":
+    main()
