54f6bd66b12e086a8680abec5592cee1e62775e8
TagRomEdu <qwertystyles@gmail.com>
2023-07-08 12:46:19 +0500

    added func get_currency_rate

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ from datetime import datetime
 API_KEY = os.getenv('API_KEY')
 CURRENCY_RATES_FILE = "currency_rates.json"
 
+def get_currency_rate(currency: str) -> float:
+    """
+    Получает курс валюты от API и возвращает его в виде float\
+    """
+    url = "https://api.apilayer.com/exchangerates_data/latest"
+    params = {"apikey": API_KEY, "base": currency}
+
+    response = requests.get(url, params)
+
+    return response.json()['rates']['RUB']
+
